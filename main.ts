bb9aae171a69d93eed7167b12eef504d350e269b
shoolacc <123706564+shoolacc@users.noreply.github.com>
2023-03-14 11:39:09 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+input.onGesture(Gesture.Shake, function () {
+    while (true) {
+        radio.sendString(GPS)
+    }
 })
+let GPS = ""
+radio.setGroup(4)
+let Latitude = 52.1818424
+let Longitude = 0.1789449
+let Tree_fall_message = "Tree has fallen"
+GPS = "" + Latitude + Longitude + Tree_fall_message
+basic.showString(GPS)
